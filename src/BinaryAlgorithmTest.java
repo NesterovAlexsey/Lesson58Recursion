22634e4e8a71ecf86551eab05205068b255d5d07
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-04-12 09:12:21 +0200

    minor tests update and FINISH

--- src/BinaryAlgorithmTest.java
+++ src/BinaryAlgorithmTest.java
@@ -1,7 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
 
 public class BinaryAlgorithmTest {
@@ -17,20 +16,20 @@ public class BinaryAlgorithmTest {
     numbers.add(7);
     numbers.add(9);
 
-    int target1 = 1;
-    int target2 = 2;
-
-    int index1 = test.myIndexOf(numbers, target1, 0, numbers.size());
-    int index2 = test.myIndexOf(numbers, target2, 0, numbers.size());
+    int index1 = test.myIndexOf(numbers, 1, 0, numbers.size());
+    int index2 = test.myIndexOf(numbers, 2, 0, numbers.size());
     int index3 = test.myIndexOf(numbers, 9, 0, numbers.size());
     int index4 = test.myIndexOf(numbers, 12, 0, numbers.size());
     int index5 = test.myIndexOf(numbers, -35, 0, numbers.size());
-
+    int index6 = test.myIndexOf(numbers, 0, 0, numbers.size());
+    int index7 = test.myIndexOf(numbers, 5, 0, numbers.size());
 
     assertEquals(0, index1);
     assertEquals(-1, index2);
     assertEquals(4, index3);
     assertEquals(-1, index4);
     assertEquals(-1, index5);
+    assertEquals(-1, index6);
+    assertEquals(2, index7);
   }
 }
